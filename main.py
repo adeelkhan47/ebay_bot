c7c26bfd28dee9273ebaa0ae94e53d06a83a4621
= <adiiiiikhan1998@outlook.com>
2023-09-22 00:43:34 +0500

    code fixed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import csv
 import time
 
+import math
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -12,9 +13,16 @@ def get_ebay_store_items(store_name):
     # options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
     driver = webdriver.Chrome(options=options)
     all_products = []
+    url = f"https://www.ebay.com/str/{store_name}?_fcid=1&_pgn=1"
+    driver.get(url)
+    WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, 'str-search__input')))
+
+    count = driver.find_element(By.CLASS_NAME, 'str-search__input').get_attribute("placeholder")
+    count = int(count.split(" ")[2])
+    total_pages = math.ceil(count / 48)
     page = 1
     try:
-        while True:
+        while page <= total_pages:
             url = f"https://www.ebay.com/str/{store_name}?_fcid=1&_pgn={page}"
             driver.get(url)
             WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, 'str-item-card')))
@@ -24,7 +32,7 @@ def get_ebay_store_items(store_name):
                 break
             for index, item in enumerate(items):
                 try:
-                    time.sleep(1.5)
+                    time.sleep(0.5)
                     price = item.find_element(By.CLASS_NAME, "str-item-card__property-displayPrice").text
                     try:
                         image_url = item.find_element(By.CLASS_NAME, "str-image").find_element(By.CSS_SELECTOR,
@@ -42,7 +50,7 @@ def get_ebay_store_items(store_name):
                         WebDriverWait(driver, 10).until(
                             EC.presence_of_element_located((By.XPATH, "//*[@id='quickviewDialogHeading']/a")))
                         # link = link.find_element(By.ID, "quickviewDialogHeading")
-                        time.sleep(1.5)
+                        time.sleep(0.5)
                         link = link.find_element(By.XPATH, "//*[@id='quickviewDialogHeading']/a").get_attribute("href")
                     except:
                         link = ""
@@ -54,7 +62,7 @@ def get_ebay_store_items(store_name):
                         'Image Url': image_url,
                     }
                     all_products.append(fetched_item)
-                    time.sleep(1.5)
+                    time.sleep(0.5)
                     driver.find_element(By.CLASS_NAME, "lightbox-dialog__close").click()
                     print(f"Page : {page} Item : {index} Passed.")
 
@@ -85,7 +93,7 @@ def get_ebay_store_items(store_name):
 
 
 if __name__ == '__main__':
-    store_name = "equiptek_labs,axiomtest"
+    store_name = "axiomtest"
     for each in store_name.split(","):
         status = get_ebay_store_items(each)
         print(f"{each} : Processed => {status}")
