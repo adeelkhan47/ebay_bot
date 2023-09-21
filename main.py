9a5e3d9233320751b490e1f12be381268257bb80
= <adiiiiikhan1998@outlook.com>
2023-09-22 00:17:53 +0500

    updated

--- main.py
+++ main.py
@@ -13,53 +13,56 @@ def get_ebay_store_items(store_name):
     driver = webdriver.Chrome(options=options)
     all_products = []
     page = 1
-    while True:
-        url = f"https://www.ebay.com/str/{store_name}?_fcid=1&_pgn={page}"
-        driver.get(url)
-        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, 'str-item-card')))
+    try:
+        while True:
+            url = f"https://www.ebay.com/str/{store_name}?_fcid=1&_pgn={page}"
+            driver.get(url)
+            WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, 'str-item-card')))
 
-        items = driver.find_elements(By.CLASS_NAME, 'str-item-card')
-        if len(items) == 0:
-            break
-        for index, item in enumerate(items):
-            try:
-                time.sleep(1.5)
-                price = item.find_element(By.CLASS_NAME, "str-item-card__property-displayPrice").text
+            items = driver.find_elements(By.CLASS_NAME, 'str-item-card')
+            if len(items) == 0:
+                break
+            for index, item in enumerate(items):
                 try:
-                    image_url = item.find_element(By.CLASS_NAME, "str-image").find_element(By.CSS_SELECTOR,
-                                                                                           "img.landscape.no-scaling.zoom").get_attribute(
-                        "src")
-                except:
-                    image_url = ""
-                description = item.find_element(By.CLASS_NAME, "str-item-card__property-title").text
-                item.click()
-
-                try:
-                    WebDriverWait(driver, 10).until(
-                        EC.presence_of_element_located((By.CLASS_NAME, "lightbox-dialog__main")))
-                    link = driver.find_element(By.CLASS_NAME, "lightbox-dialog__main")
-                    WebDriverWait(driver, 10).until(
-                        EC.presence_of_element_located((By.XPATH, "//*[@id='quickviewDialogHeading']/a")))
-                    # link = link.find_element(By.ID, "quickviewDialogHeading")
                     time.sleep(1.5)
-                    link = link.find_element(By.XPATH, "//*[@id='quickviewDialogHeading']/a").get_attribute("href")
-                except:
-                    link = ""
+                    price = item.find_element(By.CLASS_NAME, "str-item-card__property-displayPrice").text
+                    try:
+                        image_url = item.find_element(By.CLASS_NAME, "str-image").find_element(By.CSS_SELECTOR,
+                                                                                               "img.landscape.no-scaling.zoom").get_attribute(
+                            "src")
+                    except:
+                        image_url = ""
+                    description = item.find_element(By.CLASS_NAME, "str-item-card__property-title").text
+                    item.click()
 
-                fetched_item = {
-                    'Title': description,
-                    'Price': price,
-                    'Url': link,
-                    'Image Url': image_url,
-                }
-                all_products.append(fetched_item)
-                time.sleep(1.5)
-                driver.find_element(By.CLASS_NAME, "lightbox-dialog__close").click()
-                print(f"Page : {page} Item : {index} Passed.")
+                    try:
+                        WebDriverWait(driver, 10).until(
+                            EC.presence_of_element_located((By.CLASS_NAME, "lightbox-dialog__main")))
+                        link = driver.find_element(By.CLASS_NAME, "lightbox-dialog__main")
+                        WebDriverWait(driver, 10).until(
+                            EC.presence_of_element_located((By.XPATH, "//*[@id='quickviewDialogHeading']/a")))
+                        # link = link.find_element(By.ID, "quickviewDialogHeading")
+                        time.sleep(1.5)
+                        link = link.find_element(By.XPATH, "//*[@id='quickviewDialogHeading']/a").get_attribute("href")
+                    except:
+                        link = ""
 
-            except Exception as e:
-                print(f"Page : {page} Item : {index} Failed.")
-        page += 1
+                    fetched_item = {
+                        'Title': description,
+                        'Price': price,
+                        'Url': link,
+                        'Image Url': image_url,
+                    }
+                    all_products.append(fetched_item)
+                    time.sleep(1.5)
+                    driver.find_element(By.CLASS_NAME, "lightbox-dialog__close").click()
+                    print(f"Page : {page} Item : {index} Passed.")
+
+                except Exception as e:
+                    print(f"Page : {page} Item : {index} Failed.")
+            page += 1
+    except Exception:
+        print("Page Completed or Not Found.")
     driver.close()
     # The name of the CSV file you want to save
     filename = f"{store_name}_products.csv"
